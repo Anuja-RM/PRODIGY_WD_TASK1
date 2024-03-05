248154ab8687cbf9c113660d87a0fc80a643de17
Anuja-RM <145795428+Anuja-RM@users.noreply.github.com>
2024-03-05 13:56:28 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,23 @@
+function f1() {
+    document.querySelector("body").style.backgroundImage = "url('Taj_Mahal.JPG')";
+}
+
+function f2() { 
+    document.querySelector("body").style.backgroundImage = "url('desert.jpg')";  
+}
+
+function f3() {
+    document.querySelector("body").style.backgroundImage = "url('Giza.jpg')";
+}
+
+function f4() {
+    document.querySelector("body").style.backgroundImage = "url('Petra.jpg')";
+}
+
+function f5() {
+    document.querySelector("body").style.backgroundImage = "url('Rome_Colosseum_exterior_panorama.jpg')";
+}
+
+function f6() {
+    document.querySelector("body").style.backgroundImage = "url('Machu_Picchu')";
+}
